1a382c883e7b04ccd8813fc56e320f463963e815
pauldobyns <pauldobyns777@gmail.com>
2018-06-19 22:17:19 -0700

    Corrected Import
    Updataed for size dimension and corrected child float

--- CorporateActionCalendarStory/CorporateActionCalendarStory/ImportDialog.xaml.cs
+++ CorporateActionCalendarStory/CorporateActionCalendarStory/ImportDialog.xaml.cs
@@ -119,7 +119,7 @@ namespace CorporateActionCalendarStory
             ((MainWindow)Application.Current.MainWindow).parentTicker.Text = data[2];
             ((MainWindow)Application.Current.MainWindow).parentTSO.Text = data[9];
             ((MainWindow)Application.Current.MainWindow).parentFloat.Text = $"{(1 - MainWindow.ConvertToDouble(data[11]))}";
-            ((MainWindow)Application.Current.MainWindow).parentSize.Text = data[4];
+            ((MainWindow)Application.Current.MainWindow).parentSize.Text = DetermineSize(data);
             ((MainWindow)Application.Current.MainWindow).parentGrowth.Text = data[15];
             ((MainWindow)Application.Current.MainWindow).parentDynamic.Text = "";
             if (data[22] == "Y")
@@ -139,10 +139,10 @@ namespace CorporateActionCalendarStory
             ((MainWindow)Application.Current.MainWindow).childCompanyName.Text = data[4];
             ((MainWindow)Application.Current.MainWindow).childTicker.Text = data[2];
             ((MainWindow)Application.Current.MainWindow).childTSO.Text = data[9];
-            ((MainWindow)Application.Current.MainWindow).childFloat.Text = data[12];
-            ((MainWindow)Application.Current.MainWindow).childSize.Text = data[4];
+            ((MainWindow)Application.Current.MainWindow).childFloat.Text = $"{(1 - MainWindow.ConvertToDouble(data[11]))}";
+            ((MainWindow)Application.Current.MainWindow).childSize.Text = DetermineSize(data);
             ((MainWindow)Application.Current.MainWindow).childGrowth.Text = data[15];
-            ((MainWindow)Application.Current.MainWindow).childDynamic.Text = data[4];
+            ((MainWindow)Application.Current.MainWindow).childDynamic.Text = "";
             if (data[22] == "Y")
             {
                 ((MainWindow)Application.Current.MainWindow).childSP5.IsChecked = true;
@@ -151,6 +151,38 @@ namespace CorporateActionCalendarStory
             {
                 ((MainWindow)Application.Current.MainWindow).childSP5.IsChecked = false;
             }
+        }
+        public static string DetermineSize(string[] data)
+        {
+            if (data[17] == "Y" && data[19] == "Y" && data[26] == "Y")
+            {
+                return "L1";
+            }
+            else if (data[17] == "Y" && data[19] == "Y" && data[26] == "N")
+            {
+                return "L3";
+            }
+            else if (data[17] == "Y" && data[18] == "Y" && data[21] == "N")
+            {
+                return "M1";
+            }
+            else if (data[17] == "Y" && data[18] == "Y" && data[18] == "Y")
+            {
+                return "M2";
+            }
+            else if (data[20] == "Y" && data[25] == "N")
+            {
+                return "S1";
+            }
+            else if (data[20] == "Y" && data[25] == "Y")
+            {
+                return "S3";
+            }
+            else if (data[24] == "N" && data[25] == "Y")
+            {
+                return "MIC1";
+            }
+            return String.Empty;
         }
     }
 }
